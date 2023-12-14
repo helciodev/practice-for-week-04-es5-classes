e2bbe6e986b76acc1d6795d0b94ce6cec5eb232b
helcio <h.andre@giap.pl>
2023-12-14 11:01:03 +0100

    Complete phase 03

--- classes/array.js
+++ classes/array.js
@@ -1,1 +1,14 @@
-// Your code here
+Array.prototype.isEqual = function (arr) {
+  if (arr.length !== this.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== this[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+module.exports = Array;
--- classes/person.js
+++ classes/person.js
@@ -1,6 +1,6 @@
-const Dog = require('./dog');
+const Dog = require("./dog");
 
-class Person {
+/*class Person {
   constructor(name, age, dogs = []) {
     this.name = name;
     this.age = age;
@@ -20,6 +20,25 @@ class Person {
     });
   }
 }
+*/
+function Person(name, age, dogs = []) {
+  this.name = name;
+  this.age = age;
+  this.dogs = dogs;
+}
+
+Person.prototype.addDog = function (dog) {
+  if (!(dog instanceof Dog)) {
+    throw new TypeError("Can only add dogs!");
+  }
+  this.dogs.push(dog);
+};
+
+Person.prototype.walkDogs = function () {
+  this.dogs.forEach((dog) => {
+    console.log(`walking ${dog.name}`);
+  });
+};
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
